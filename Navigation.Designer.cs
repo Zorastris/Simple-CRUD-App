ecf5ebd40cf6e7bdf8fda00860d04bb50e2e7670
Batu Büktel <batubuktel@gmail.com>
2020-11-06 17:03:03 +0300

    Add project files.

---
+++ Navigation.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SimpleDataApp
+{
+    partial class Navigation
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGoToAdd = new System.Windows.Forms.Button();
+            this.btnGoToFillOrCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(65, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "What do you want to do?";
+            // 
+            // btnGoToAdd
+            // 
+            this.btnGoToAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnGoToAdd.Location = new System.Drawing.Point(98, 132);
+            this.btnGoToAdd.Name = "btnGoToAdd";
+            this.btnGoToAdd.Size = new System.Drawing.Size(164, 31);
+            this.btnGoToAdd.TabIndex = 1;
+            this.btnGoToAdd.Text = "Add an account";
+            this.btnGoToAdd.UseVisualStyleBackColor = true;
+            this.btnGoToAdd.Click += new System.EventHandler(this.btnGoToAdd_Click);
+            // 
+            // btnGoToFillOrCancel
+            // 
+            this.btnGoToFillOrCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnGoToFillOrCancel.Location = new System.Drawing.Point(50, 169);
+            this.btnGoToFillOrCancel.Name = "btnGoToFillOrCancel";
+            this.btnGoToFillOrCancel.Size = new System.Drawing.Size(239, 31);
+            this.btnGoToFillOrCancel.TabIndex = 1;
+            this.btnGoToFillOrCancel.Text = "Fill or cancel an order";
+            this.btnGoToFillOrCancel.UseVisualStyleBackColor = true;
+            this.btnGoToFillOrCancel.Click += new System.EventHandler(this.btnGoToFillOrCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExit.Location = new System.Drawing.Point(284, 302);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(96, 31);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Navigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 374);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGoToFillOrCancel);
+            this.Controls.Add(this.btnGoToAdd);
+            this.Controls.Add(this.label1);
+            this.Name = "Navigation";
+            this.Text = "Welcome";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGoToAdd;
+        private System.Windows.Forms.Button btnGoToFillOrCancel;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ NewCustomer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SimpleDataApp
+{
+
+    public partial class NewCustomer : Form
+    {
+        private int parsedCustomerID;
+        private int orderID;
+
+        private bool IsCustomerNameValid()
+        {
+            if (txtCustomerName.Text == "")
+            {
+                MessageBox.Show("Please enter a name.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool IsOrderDataValid()
+        {
+            // Verify that CustomerID is present.
+            if (txtCustomerID.Text == "")
+            {
+                MessageBox.Show("Please create customer account before placing order.");
+                return false;
+            }
+            // Verify that Amount isn't 0.
+            else if ((numOrderAmount.Value < 1))
+            {
+                MessageBox.Show("Please specify an order amount.");
+                return false;
+            }
+            else
+            {
+                // Order can be submitted.
+                return true;
+            }
+        }
+
+        public NewCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void dtpOrderDate_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewCustomer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddAnotherAccount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddFinish_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlaceOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
